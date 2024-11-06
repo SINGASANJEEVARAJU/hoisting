f833fec11882450dd4b387089d13cab0f5d22aa3
SINGA SANJEEVARAJU <sanjeevaraj123@gmail.com>
2024-11-06 22:43:38 +0530

    files rae added

---
+++ day2.js
@@ -0,0 +1,142 @@
+function a(){
+    console.log("surya")
+}
+a()
+function a(x){
+    console.log(x)
+}
+a(10)
+function a(x,y){
+    console.log(x)
+}
+a(10,"dad")
+function a(x,y,z){
+    console.log(x+y+z)
+}
+// a(30,40,50)
+a('vamsi','surya','karna')
+function a(x,y,z,e){
+    console.log(typeof (e))
+}
+a(10,20,30,"")
+a ("surya",30,"true","raju")
+function a(x,y,z,e){
+    console.log(x+y+z+e)
+}
+a ("surya",30,"true","raju")
+function a(x,y,z,e){
+    console.log(x+y+z+e)
+    console.log(x+y+z+e)
+}
+console.log(a)
+
+var a =100;
+console.log(a)
+
+let a =100;
+function a(){
+    var z=100;
+    console.log(z)
+}
+a()
+function a(){
+    var a=100;
+            b()
+    function b(){
+        console.log(a)
+    }
+}
+a()
+var z=100;
+function a(){
+    console.log(z)
+    b()
+    function b(){
+        var a=20
+    }
+    return b
+}
+a()
+function a(){
+    b()
+    function b(){
+
+    }
+    var z=100
+    console.log(z)
+}
+a()
+function a(){
+    b()
+    function b(){
+        console.log(z)
+    }
+}
+a()
+let z=100;
+function a(){
+    b()
+        function b(){
+            console.log(z)
+        }
+}
+a()
+
+function a(){
+    b()
+        function b(){
+            (c)
+           function (c){
+            let z=100;
+            console.log(z)
+           }
+        }
+}
+a()
+function a(x,y){
+    console.log(x+y)
+}
+a(10,20)
+function a(x,y){
+    return x+y;
+    console.log("hello")
+}
+a(10,20)
+function a(x,y){
+    return console.log(x+y)
+}
+a(10,20)
+function a(){
+    return b()
+    function b(){
+        console.log(z)
+            var z=100;
+    }
+    }
+
+a()
+function a(){
+    function b(){
+        console.log(z)
+        var z=100;
+    
+}return b()
+}
+a()
+
+var c=100;
+function a(){
+    function b(){
+        console.log(c)
+    }
+    return b();
+    const z=a()
+console.log(z)
+}
+a()
+setTimeout (function a(){
+    console.log("a function");
+},2000)
+setInterval (function a(){
+    console.log("function is consequently invoked")
+},3000)
